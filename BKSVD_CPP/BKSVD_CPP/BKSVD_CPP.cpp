58a88522794846e9920e3eaf3975681791353691
博 黄 <huangbo19971109@outlook.com>
2019-02-27 21:58:34 +0800

    Thinking about how to do block change

--- BKSVD_CPP/BKSVD_CPP/BKSVD_CPP.cpp
+++ BKSVD_CPP/BKSVD_CPP/BKSVD_CPP.cpp
@@ -1,24 +1,85 @@
-﻿// BKSVD_CPP.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿/*
+BKSVD implementing by C++ based on https://github.com/cpmusco/bksvd
+*/
 
 #include <bits/stdc++.h>
 #include <Eigen/Dense>
+#include <random>
+#include <ctime>
 
 using namespace std;
 using namespace Eigen;
 
-int main(){
-	MatrixXd m = MatrixXd::Random(3, 3);
-	cout << m << endl;
+struct bksvd_output {
+	MatrixXd U;	// an orthogonal matrix whose columns are approximate top k left singular vectors for A
+	MatrixXd S; // a diagonal matrix whose entries are A's approximate top k singular values
+	MatrixXd V; // an orthogonal matrix whose columns are approximate top k right singular vectors for A
+};
+
+bksvd_output bksvd(MatrixXd A, int k = 6, int iter = 3, int bsize = 6, bool center = false) {
+	bksvd_output result;
+	static default_random_engine e((unsigned int)time(0));
+	static normal_distribution<double> m_n(0, 1);
+	MatrixXd u = MatrixXd::Zero(1, A.cols());
+
+	//	Calculate row mean if rows should be centered.
+	if (center) {
+		for (int i = 0; i < A.cols(); i++) {
+			u(0, i) = A.col(i).mean();
+		}
+	}
+	MatrixXd l = MatrixXd::Ones(A.rows(), 1);
+
+	//	We want to iterate on the smaller dimension of A.
+	int n, ind;
+	if (A.rows() <= A.cols()) {
+		n = A.rows();
+		ind = 1;
+	}
+	else {
+		n = A.cols();
+		ind = 2;
+	}
+	bool tpose = false;
+	if (ind == 1) {
+		tpose = true;
+		l = u.transpose(); u.setOnes(1, A.rows());
+		A = A.transpose();
+	}
+
+	//	Allocate space for Krylov subspace.
+	MatrixXd K = MatrixXd::Zero(A.cols(), bsize * iter);
+	//	Random block initialization.
+	MatrixXd block = MatrixXd::Zero(A.cols(), bsize).unaryExpr([](double dummy) {return m_n(e); });
+	HouseholderQR<MatrixXd> qr;
+	qr.compute(block);
+	block = qr.householderQ();
+	MatrixXd R = qr.matrixQR().triangularView<Upper>();	// [block, R] = qr(block, 0), But this is not simplified QR
+
+	//	Preallocate space for temporary products.
+	MatrixXd T = MatrixXd::Zero(A.cols(), bsize);
+
+	//	Construct and orthonormalize Krlov Subspace.
+	//	Orthogonalize at each step using economy size QR decomposition
+	for (int i = 1; i <= iter; i++) {
+		T = A * block - l * (u * block);
+		block = A.transpose() * T - u.transpose() * (l.transpose() * T);
+		HouseholderQR<MatrixXd> tmp_qr;
+		qr.compute(block);
+		block = qr.householderQ();
+		R = qr.matrixQR().triangularView<Upper>();
+		
+	}
+	return result;
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
 
-// 入门提示: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+int main(){
+	MatrixXd A(4, 3);
+	A(0, 0) = 0; A(0, 1) = 1; A(0, 2) = 1;
+	A(1, 0) = 2; A(1, 1) = 3; A(1, 2) = 2;
+	A(2, 0) = 1; A(2, 1) = 3; A(2, 2) = 2;
+	A(3, 0) = 4; A(3, 1) = 2; A(3, 2) = 2;
+	bksvd(A);
+	cout << "Done." << endl;
+}
