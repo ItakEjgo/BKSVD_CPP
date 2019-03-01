13f18f29a4f9a32aa91743f48e0b7035c0e8357b
博 黄 <huangbo19971109@outlook.com>
2019-03-01 19:37:32 +0800

    sisvd added

--- BKSVD_CPP/BKSVD_CPP/BKSVD_CPP.cpp
+++ BKSVD_CPP/BKSVD_CPP/BKSVD_CPP.cpp
@@ -12,13 +12,16 @@ using namespace std;
 using namespace Eigen;
 
 struct bksvd_output {
-	MatrixXd U;	// an orthogonal matrix whose columns are approximate top k left singular vectors for A
-	MatrixXd S; // a diagonal matrix whose entries are A's approximate top k singular values
-	MatrixXd V; // an orthogonal matrix whose columns are approximate top k right singular vectors for A
+	MatrixXd U;		// an orthogonal matrix whose columns are approximate top k left singular vectors for A
+	MatrixXd S;		// a diagonal matrix whose entries are A's approximate top k singular values
+	MatrixXd V;		// an orthogonal matrix whose columns are approximate top k right singular vectors for A
 };
+
+//	function bksvd
 
 bksvd_output bksvd(MatrixXd A, int k = 6, int iter = 3, int bsize = 6, bool center = false) {
 	bksvd_output result;
+
 	static default_random_engine e((unsigned int)time(0));
 	static normal_distribution<double> m_n(0, 1);
 	MatrixXd u = MatrixXd::Zero(1, A.cols());
@@ -94,6 +97,78 @@ bksvd_output bksvd(MatrixXd A, int k = 6, int iter = 3, int bsize = 6, bool cent
 	return result;
 }
 
+//	function sisvd
+
+bksvd_output sisvd(MatrixXd A, int k = 6, int iter = 3, int bsize = 6, bool center = false) {
+	bksvd_output result;
+
+	static default_random_engine e((unsigned int)time(0));
+	static normal_distribution<double> m_n(0, 1);
+	MatrixXd u = MatrixXd::Zero(1, A.cols());
+	k = min(k, min(A.rows(), A.cols()));
+	bsize = k;
+	//	Calculate row mean if rows should be centered.
+	if (center) {
+		for (int i = 0; i < A.cols(); i++) {
+			u(0, i) = A.col(i).mean();
+		}
+	}
+	MatrixXd l = MatrixXd::Ones(A.rows(), 1);
+
+	//	We want to iterate on the smaller dimension of A.
+	int n, ind;
+	if (A.rows() <= A.cols()) {
+		n = A.rows();
+		ind = 1;
+	}
+	else {
+		n = A.cols();
+		ind = 2;
+	}
+	bool tpose = false;
+	if (ind == 1) {
+		tpose = true;
+		l = u.adjoint(); u.setOnes(1, A.rows());
+		A = A.adjoint();
+	}
+
+	//	Random block initialization.
+	MatrixXd block = MatrixXd::Zero(A.cols(), bsize).unaryExpr([](double dummy) {return m_n(e); });
+	HouseholderQR<MatrixXd> qr;
+	qr.compute(block);
+	block = qr.householderQ();
+	MatrixXd R = qr.matrixQR().triangularView<Upper>();	// [block, R] = qr(block, 0), But this is not simplified QR
+
+	//	Preallocate space for temporary products.
+	MatrixXd T = MatrixXd::Zero(A.cols(), bsize);
+
+	//	Construct and orthonormalize Krlov Subspace.
+	//	Orthogonalize at each step using economy size QR decomposition
+	for (int i = 1; i <= iter; i++) {
+		T = A * block - l * (u * block);
+		block = A.adjoint() * T - u.adjoint() * (l.adjoint() * T);
+		HouseholderQR<MatrixXd> tmp_qr;
+		tmp_qr.compute(block);
+		block = tmp_qr.householderQ();
+		R = tmp_qr.matrixQR().triangularView<Upper>();
+	}
+
+	//	Rayleigh-Ritz postprocessing with economy size dense SVD.
+	T = A * block - l * (u * block);
+	JacobiSVD<MatrixXd>svd(T, ComputeFullU | ComputeFullV);
+	MatrixXd Ut = svd.matrixU(), Vt = svd.matrixV();
+	MatrixXd St = Ut.inverse() * T * Vt.transpose().inverse();
+	result.S = St.block(0, 0, k, k);
+	if (!tpose) {
+		result.U = Ut.leftCols(k);
+		result.V = block * Vt.leftCols(k);
+	}
+	else {
+		result.V = Ut.leftCols(k);
+		result.U = block * Vt.leftCols(k);
+	}
+	return result;
+}
 
 int main(){
 	MatrixXd A = MatrixXd::Random(8, 6);
@@ -102,9 +177,15 @@ int main(){
 	start = clock();
 	result = bksvd(A);
 	end = clock();
-	cout << "U = \n" << result.U << endl;
-	cout << "S = \n" << result.S << endl;
-	cout << "V = \n" << result.V << endl;
-	cout << "Done." << endl;
-	printf("Time used = :%.5f\n", (double)(end - start) / CLOCKS_PER_SEC);
+	cout << "bksvd U = \n" << result.U << endl;
+	cout << "bksvd S = \n" << result.S << endl;
+	cout << "bksvd V = \n" << result.V << endl;
+	printf("Time used = :%.5f second(s)\n", (double)(end - start) / CLOCKS_PER_SEC);
+	start = clock();
+	result = sisvd(A);
+	end = clock();
+	cout << "sisvd U = \n" << result.U << endl;
+	cout << "sisvd S = \n" << result.S << endl;
+	cout << "sisvd V = \n" << result.V << endl;
+	printf("Time used = :%.5f second(s)\n", (double)(end - start) / CLOCKS_PER_SEC);
 }
